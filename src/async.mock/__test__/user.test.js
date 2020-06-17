4e9261e33e8527d819e71bdcb36a87e7c33975e7
jingx <xin.jing1@thoughtworks.com>
2020-06-17 19:04:15 +0800

    [xin] finish all

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,25 @@
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
-    // TODO 19: add test here
+  test("should post user when validated", async () => {
+    const mockUsername = "mocked userName";
+    const mockPwd = "mocked Pwd";
+
+    await expect(register(mockUsername, mockPwd)).resolves.toEqual({});
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockReturnValue(false);
+    const mockPwd = "mocked Pwd";
+    const mockUsername = "mocked userName";
+
+    await expect(register(mockUsername, mockPwd)).rejects.toThrowError(
+      Error("wrong username or password")
+    );
   });
 });
--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,14 +1,8 @@
-import mockAxios from "axios";
 import getUsers from "../users";
 
 jest.mock("axios");
 describe("users", () => {
   test("should get users data with mock axios get", async () => {
-    mockAxios.get.mockImplementationOnce(() =>
-      Promise.resolve({
-        data: {},
-      })
-    );
     await expect(getUsers()).resolves.toEqual({});
   });
 });
