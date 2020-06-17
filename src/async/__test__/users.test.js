77373a1fe1ac7d2d5ab119ddb6351bac5ef6160b
jingx <xin.jing1@thoughtworks.com>
2020-06-17 14:48:00 +0800

    [xin] finish TODO 1-13

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,14 @@
+import mockAxios from "axios";
+import getUsers from "../users";
+
+jest.mock("axios");
 describe("users", () => {
-  test("should get users data with mock axios get", () => {
-    // TODO 13: add async test with manual mock
+  test("should get users data with mock axios get", async () => {
+    mockAxios.get.mockImplementationOnce(() =>
+      Promise.resolve({
+        data: {},
+      })
+    );
+    await expect(getUsers()).resolves.toEqual({});
   });
 });
